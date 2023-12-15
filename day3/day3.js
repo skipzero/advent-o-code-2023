5fd7036cc9948076b45906c77aac69ed22a56cfb
skipZero <skip@angerbunny.com>
2023-12-15 15:35:19 -0800

    day3  ..

--- day3/day3.js
+++ day3/day3.js
@@ -3,7 +3,13 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
 
 function findParts () {
-  let partsArray  [];
+  let partsArray = [];
 
-  
+  let dataArr = data.split('\n')
+  dataArr = dataArr.map(line => {
+    return line.split()
+  })
+  console.log(dataArr)
 }
+
+findParts()
