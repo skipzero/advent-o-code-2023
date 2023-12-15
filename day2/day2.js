4f6cad93d293d41e530c26ff765cfdc3c3f32353
Skip McBride <skip@zerosquadron.com>
2023-12-15 15:03:51 -0800

    pulling from gh

--- day2/day2.js
+++ day2/day2.js
@@ -1,22 +1,45 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const myHand = {
-  r: 12,
-  g: 13,
-  b: 14
-}
+  red: 12,
+  green: 13,
+  blue: 14,
+};
 
 function cubeGame() {
-  const data = fs.readFileSync('input2.txt', 'utf8');
-  const dataArray = data.split('\n')[0]
-    .reduce((acc, row) => {
-    acc = row.split(';')
-    return accd
-  }, {})  
-  console.log(gameArrayVal)
-  return gameArrayVal
-  
-s
+  const data = fs.readFileSync("input2.txt", "utf8");
+  let dataArray = data.split("\n");
+  dataArray = dataArray.map((line) => {
+    let lineArray = line.split(":");
+    lineArray = lineArray[1].split(";");
+    console.log("--", line);
+    return lineArray.map((game) => {
+      game = game.split(",");
+      return game.reduce((acc, hand) => {
+        hand = hand.trim().split(" ");
+        console.log("hand", hand);
+        acc[hand[1]] = parseInt(hand[0]);
+        return acc;
+      }, {});
+    });
+  });
+  console.log(">>>>", dataArray);
+
+  const sum = dataArray.reduce((acc, hands, index) => {
+    let myArray = [];
+    console.log("HAND:::", hands);
+    hands.forEach((hand) => {
+      myArray;
+      for (const color in hand) {
+        console.log("MY COLOURS", hand[color], myHand[color]);
+        if (hand[color] < myHand[color]) {
+          acc += index + 1;
+        }
+      }
+    });
+    return acc;
+  }, 0);
+  console.log("SUM:::", sum);
 }
 
-cubeGame()
+cubeGame();
